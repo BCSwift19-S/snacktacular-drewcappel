31539338f21c9fc5dfbf2cb41bbaeb8dfe65524c
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-04-01 00:50:38 -0400

    9.3 Complete

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,15 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Drew Cappel on 3/31/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
+
+
